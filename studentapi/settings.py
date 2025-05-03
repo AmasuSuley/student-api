6b6edfa9c5629c744bd35f9e8fc16b28bd293f0d
AmasuSuley <154750020+AmasuSuley@users.noreply.github.com>
2025-05-04 01:13:16 +0300

    Update settings.py

--- studentapi/settings.py
+++ studentapi/settings.py
@@ -27,6 +27,11 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",         
+    
+]
+
 
 # Application definition
 
@@ -38,9 +43,11 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'core',
+     'corsheaders',
 ]
 
 MIDDLEWARE = [
+     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
